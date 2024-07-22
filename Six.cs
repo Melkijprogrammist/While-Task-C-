2e3c86faaea8eae6cface67f8840458dfbc719ce
Uliana <uliana20037@yandex.ru>
2024-07-22 19:22:24 +0300

    Create Six.cs

---
+++ Six.cs
@@ -0,0 +1,29 @@
+## Напечатать ряд чисел 20 в виде:
+
+# 20 20 20 20 20 20 20 20 20 20.
+  # 1
+  using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(20);
+        }
+        }
+    }
+# 2
+  using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine(40);
+        }
+        }
+    }
