64777a9173740c54d1b3ce908d84462158c8303a
Uliana <uliana20037@yandex.ru>
2024-07-22 19:35:49 +0300

    Create eight.cs

---
+++ eight.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.WriteLine($"{i} * 9 = { i *  9}");
+        }
+    }
+}
